729b774e2f206478598aa445d599429a3a7844e9
hanyelC <hanyel.chamon@gmail.com>
2022-07-17 10:58:54 -0300

    feat(productcategorycreateservice): create service

---
+++ src/api/services/products/ProductCategoryCreateService.js
@@ -0,0 +1,33 @@
+const { AppError } = require('../../utils/AppError')
+
+class ProductCategoryCreateService {
+  constructor(repository) {
+    this.repository = repository
+  }
+
+  execute({ name, description }) {
+    if (typeof name != 'string') {
+      throw new AppError('Nome da categoria deve ser um texto.')
+    }
+
+    if (!name) {
+      throw new AppError('Nome da categoria é obrigatório.')
+    }
+
+    if (name.length > 50) {
+      throw new AppError('Nome da categoria não deve ter mais que 50 caracteres.')
+    }
+
+    const categoryWithName = await this.repository.findByName(name)
+
+    if (categoryWithName) {
+      throw new AppError('Já existe uma categoria com esse nome.')
+    }
+
+    const categoryCreated = await this.repository.create({ name, description })
+
+    return { id: categoryCreated.id }
+  }
+}
+
+module.exports = { ProductCategoryCreateService }

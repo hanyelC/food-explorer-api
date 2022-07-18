321607df1c147951820fb6da3145fe24ed0fbce3
hanyelC <hanyel.chamon@gmail.com>
2022-07-18 10:36:52 -0300

    test(ingredientcreateservice): create tests and repository for tests

---
+++ src/api/repositories/IngredientsRepositoryInMemory.js
@@ -0,0 +1,41 @@
+class IngredientsRepositoryInMemory {
+  constructor(ingredients = []) {
+    this.ingredients = ingredients
+  }
+
+  async index() {
+    return this.ingredients
+  }
+
+  async findById(id) {
+    const ingredient = this.ingredients.find(ing => ing.id === id)
+    return ingredient
+  }
+
+  async findByName(name) {
+    const ingredient = this.ingredients.find(ing => ing.name === name)
+    return ingredient
+  }
+
+  async create({ name, image }) {
+    const newIngredient = {
+      id: Math.floor(Math.random() * 1E9),
+      name,
+      image
+    }
+
+    this.ingredients.push(newIngredient)
+
+    return { id: newIngredient.id }
+  }
+
+  async update({ name, image }) {
+
+  }
+
+  async delete(id) {
+
+  }
+}
+
+module.exports = { IngredientsRepositoryInMemory }
---
+++ src/api/services/ingredients/__tests__/IngredientCreateService.test.js
@@ -0,0 +1,73 @@
+const { IngredientCreateService } = require('../IngredientCreateService')
+const { IngredientsRepositoryInMemory } = require('../../../repositories/IngredientsRepositoryInMemory')
+const { AppError } = require('../../../utils/AppError')
+
+describe('IngredientCreateService', () => {
+  let ingredient
+  let ingredientsRepositoryInMemory
+  let ingredientCreateService
+
+  beforeEach(() => {
+    ingredient = {
+      name: 'foo',
+      image: '0101010101100101010'
+    }
+
+    ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory()
+    ingredientCreateService = new IngredientCreateService(ingredientsRepositoryInMemory)
+  })
+
+  it('Ingredient should be created with valid data', async () => {
+    const createdIngredient = await ingredientCreateService.execute(ingredient)
+
+    expect(createdIngredient).toHaveProperty('id')
+  })
+
+  it('Ingredient should not be created without name', async () => {
+    delete ingredient.name
+
+    await expect(ingredientCreateService.execute(ingredient))
+      .rejects
+      .toEqual(new AppError('Nome do ingrediente é obrigatório.'))
+  })
+
+  it('Ingredient should not be created with invalid name => []', async () => {
+    ingredient.name = []
+
+    await expect(ingredientCreateService.execute(ingredient))
+      .rejects
+      .toEqual(new AppError('Nome do ingrediente deve ser um texto.'))
+  })
+
+  it('Ingredient should not be created with invalid name => {}', async () => {
+    ingredient.name = {}
+
+    await expect(ingredientCreateService.execute(ingredient))
+      .rejects
+      .toEqual(new AppError('Nome do ingrediente deve ser um texto.'))
+  })
+
+  it('Ingredient should not be created with invalid name => 1', async () => {
+    ingredient.name = 1
+
+    await expect(ingredientCreateService.execute(ingredient))
+      .rejects
+      .toEqual(new AppError('Nome do ingrediente deve ser um texto.'))
+  })
+
+  it('Ingredient should not be created with name that already exists', async () => {
+    await ingredientCreateService.execute(ingredient)
+
+    await expect(ingredientCreateService.execute(ingredient))
+      .rejects
+      .toEqual(new AppError('Já existe um ingrediente cadastrado com esse nome.'))
+  })
+
+  it('Ingredient should not be created without image', async () => {
+    delete ingredient.image
+
+    await expect(ingredientCreateService.execute(ingredient))
+      .rejects
+      .toEqual(new AppError('Imagem do ingrediente é obrigatória.'))
+  })
+})

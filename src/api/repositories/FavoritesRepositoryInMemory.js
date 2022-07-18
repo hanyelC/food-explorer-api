b8fb1962ad31ddb6143c5ff2416cee76e3af2465
hanyelC <hanyel.chamon@gmail.com>
2022-07-18 15:05:46 -0300

    test(favoritecreateservice): create test suite and repository for tests

---
+++ src/api/repositories/FavoritesRepositoryInMemory.js
@@ -0,0 +1,43 @@
+class FavoritesRepositoryInMemory {
+  constructor(favorites = []) {
+    this.favorites = favorites
+  }
+
+  async index() {
+    return this.favorites
+  }
+
+  async findById(user_id, product_id) {
+    const id = Number('' + user_id + product_id)
+
+    const favorite = this.favorites.find(fav => fav.id === id)
+
+    return favorite
+  }
+
+  async findByUserId(user_id) {
+
+  }
+
+  async findByProductId(product_id) {
+
+  }
+
+  async create(user_id, product_id) {
+    const favorite = {
+      id: Number('' + user_id + product_id),
+      user_id,
+      product_id
+    }
+
+    this.favorites.push(favorite)
+
+    return { id: favorite.id }
+  }
+
+  async delete(user_id, product_id) {
+
+  }
+}
+
+module.exports = { FavoritesRepositoryInMemory }
---
+++ src/api/services/favorites/__tests__/FavoriteCreateService.test.js
@@ -0,0 +1,45 @@
+const { FavoriteCreateService } = require('../FavoriteCreateService')
+const { FavoritesRepositoryInMemory } = require('../../../repositories/FavoritesRepositoryInMemory')
+const { AppError } = require('../../../utils/AppError')
+
+describe('FavoriteCreateService', () => {
+  let user_id
+  let product_id
+  let favoriteCreateService
+  let favoritesRepositoryInMemory
+
+  beforeEach(() => {
+    user_id = 1
+    product_id = 1
+
+    favoritesRepositoryInMemory = new FavoritesRepositoryInMemory()
+    favoriteCreateService = new FavoriteCreateService(favoritesRepositoryInMemory)
+  })
+
+  it('Favorite should be created with valid data', async () => {
+    const createdFavorite = await favoriteCreateService.execute({ user_id, product_id })
+
+    expect(createdFavorite).toHaveProperty('id')
+    expect(createdFavorite.id).toBe(11)
+  })
+
+  it('Favorite should not be created without user id', async () => {
+    await expect(favoriteCreateService.execute({ product_id }))
+      .rejects
+      .toEqual(new AppError('User id is required.'))
+  })
+
+  it('Favorite should not be created without product id', async () => {
+    await expect(favoriteCreateService.execute({ user_id }))
+      .rejects
+      .toEqual(new AppError('Product id is required.'))
+  })
+
+  it('Favorite that already exists should not be created', async () => {
+    await favoriteCreateService.execute({ user_id, product_id })
+
+    await expect(favoriteCreateService.execute({ user_id, product_id }))
+      .rejects
+      .toEqual(new AppError('Favorito já existe.'))
+  })
+})

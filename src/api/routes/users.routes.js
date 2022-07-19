dc714fbbba7ea7587be70a0839eabffa673b6520
hanyelC <hanyel.chamon@gmail.com>
2022-07-19 11:14:32 -0300

    feat(usersrouter): add POST route

--- src/api/routes/users.routes.js
+++ src/api/routes/users.routes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
+const { UsersController } = require('../controllers/UsersController')
 
 const userRouter = Router()
+const usersController = new UsersController()
 
-userRouter.post('/', /*users Controller*/)
+userRouter.post('/', usersController.create)
 
 module.exports = { userRouter }

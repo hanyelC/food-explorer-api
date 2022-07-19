9b6ce6a4b9a50ea1677e259422b7a5e18243443b
hanyelC <hanyel.chamon@gmail.com>
2022-07-19 09:54:07 -0300

    build: running server on port from env variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 require('express-async-errors')
 
 const app = require('./config/express')
 
-app.listen(3030, () => console.log('server started on port 3030'))
+app.listen(process.env.PORT || 3030, () => console.log('server started on port ', process.env.PORT || 3030))

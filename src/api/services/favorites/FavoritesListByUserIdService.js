21d3a32bf389c4af36a02d882d40bac1a91035aa
hanyelC <hanyel.chamon@gmail.com>
2022-07-19 16:25:35 -0300

    feat(services|favorites): create ListByUserIdService

---
+++ src/api/services/favorites/FavoritesListByUserIdService.js
@@ -0,0 +1,23 @@
+const { AppError } = require('../../utils/AppError')
+
+class FavoritesListByUserIdService {
+  constructor(repository) {
+    this.repository = repository
+  }
+
+  async execute(user_id) {
+    if (!user_id) {
+      throw new AppError('User id is required.')
+    }
+
+    if (typeof user_id != 'number') {
+      throw new AppError('User id should be a number.')
+    }
+
+    const favorites = await this.repository.findByUserId(user_id)
+
+    return favorites
+  }
+}
+
+module.exports = { FavoritesListByUserIdService }

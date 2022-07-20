30485ea64cd735f44e367d5fbcdfca725a24947e
hanyelC <hanyel.chamon@gmail.com>
2022-07-20 09:18:20 -0300

    feat(products.routes): add more routes

--- src/api/routes/products.routes.js
+++ src/api/routes/products.routes.js
@@ -1,7 +1,18 @@
 const { Router } = require('express')
+const { ProductsController } = require('../controllers/ProductsController')
 
 const productsRouter = Router()
+const productsController = new ProductsController()
 
-productsRouter.post('/', /*Product controller*/)
+// logged users only
+
+productsRouter.get('/', productsController.index)
+productsRouter.get('/:product_id', productsController.show)
+
+// admin only
+
+productsRouter.post('/', productsController.create)
+productsRouter.put('/', productsController.update)
+productsRouter.delete('/:product_id', productsController.delete)
 
 module.exports = { productsRouter }

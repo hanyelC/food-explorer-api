c4ff272b713536f4bda8cc1094b7f54571212a4e
hanyelC <hanyel.chamon@gmail.com>
2022-07-20 09:27:09 -0300

    feat(productdeleteservice): create the service

---
+++ src/api/services/products/ProductDeleteService.js
@@ -0,0 +1,23 @@
+const { AppError } = require('../../utils/AppError')
+
+class ProductDeleteService {
+  constructor(repository) {
+    this.repository = repository
+  }
+
+  async execute(product_id) {
+    if (!product_id) {
+      throw new AppError('Product id is required.')
+    }
+
+    if (typeof product_id != 'number') {
+      throw new AppError('Product id should be a number')
+    }
+
+    const deletedItem = await this.repository.delete(product_id)
+
+    return { id: deletedItem.id }
+  }
+}
+
+module.exports = { ProductDeleteService }

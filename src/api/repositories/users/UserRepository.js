d9c2f0e84dd2e3d5d3434b3960eabc4c8e2a4d93
hanyelC <hanyel.chamon@gmail.com>
2022-08-04 17:40:11 -0300

    feat(repositories): create UserRepository

--- src/api/repositories/users/UserRepository.js
+++ src/api/repositories/users/UserRepository.js
@@ -1,1 +1,28 @@
-// TODO: create repository to access postgreSQL database
+const { prisma } = require('../../lib/prisma')
+
+class UserRepository {
+
+  async findByEmail(email) {
+    const user = await prisma.user.findFirst({
+      where: {
+        email
+      }
+    })
+
+    return user
+  }
+
+  async create({ name, email, password }) {
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password
+      }
+    })
+
+    return newUser
+  }
+}
+
+module.exports = { UserRepository }

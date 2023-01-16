636c3f353999bfb51cd4b452af2522c48f550127
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-01-16 11:09:53 -0300

    fix: send image file with ingredient data

--- src/api/controllers/IngredientsController.js
+++ src/api/controllers/IngredientsController.js
@@ -1,9 +1,26 @@
 const { IngredientsRepository } = require('../repositories/ingredients/IngredientsRepository')
 const { IngredientCreateService } = require('../services/ingredients/IngredientCreateService')
+const { ImagesRepository } = require('../repositories/images/ImagesRepository')
+const { DiskStorage } = require('../../providers/DiskStorage')
 
 class IngredientsController {
   async create(req, res) {
-    const { name, image_id } = req.body
+    const { filename, mimetype } = req.file
+    const { name } = req.body
+
+    const { deleteTempFile, getTempFile } = new DiskStorage()
+
+    const { buffer } = await getTempFile(filename)
+    await deleteTempFile(filename)
+
+    const imageRepository = new ImagesRepository()
+
+    const { id: image_id } = await imageRepository.create({
+      id: filename.split('.')[0],
+      image_buffer: buffer,
+      image_name: filename,
+      image_type: mimetype,
+    })
 
     const repository = new IngredientsRepository()
     const ingredientCreateService = new IngredientCreateService(repository)
--- src/api/routes/ingredients.routes.js
+++ src/api/routes/ingredients.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express')
 const { IngredientsController } = require('../controllers/IngredientsController')
 const { Auth } = require('../middlewares/auth')
+const multer = require('multer')
+const { MULTER: multer_configs } = require('../../config/upload')
+
+const upload = multer(multer_configs)
 
 const ingredientsRouter = Router()
 const ingredientsController = new IngredientsController()
 
-ingredientsRouter.post('/', ingredientsController.create)
+ingredientsRouter.post('/', upload.single('image'), ingredientsController.create)
 
 module.exports = { ingredientsRouter }
